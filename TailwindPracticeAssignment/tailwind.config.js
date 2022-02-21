2f09e7e9b4fa99c4db8991114bf2f4ed926bff3b
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-21 15:04:01 +0200

    style: :lipstick: Added header

--- TailwindPracticeAssignment/tailwind.config.js
+++ TailwindPracticeAssignment/tailwind.config.js
@@ -1,11 +1,21 @@
+const { randHsl } = require("@ngneat/falso")
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
 
-
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif']
+       },
       gridTemplateRows: {
         home: `100px 1fr`,
+      },
+      colors:{
+        'primary': '#0d6cff',
+        'body': '#f0f0f0',
+        'secondary': '#6a6a6b',
+        'glow': '#c1ddfa',
       },
 
     },
