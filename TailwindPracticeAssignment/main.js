027485fe2a4aea144e63c9d925d0a93a0ea1a660
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-22 14:52:19 +0200

    style: :lipstick: Tweaked Home page img

--- TailwindPracticeAssignment/main.js
+++ TailwindPracticeAssignment/main.js
@@ -2,4 +2,6 @@ import './style.css';
 
 import { rand, randImg } from '@ngneat/falso';
 
-document.getElementById('aboutIMG').innerHTML = `<img src="${randImg(69)}" alt="random image"></img>`;
+let img = `https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3308&q=80`
+
+document.getElementById('aboutIMG').innerHTML = `<img class="object-fit -mt-14" src="${img}" alt="random image"></img>`;
